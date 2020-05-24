496bd9f127b8a706de1226b1a27fad24345e658d
Kim Bao <kimbao756@yahoo.com>
2020-05-24 15:17:34 +0700

    deploy storybook on firebase

---
+++ .out/public/precache-manifest.7596644416f6ab090d3412796a300b81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5239ff619e21c63b59bd378c9fd6f8d5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.801dfec724a2924f9e8f.bundle.js"
+  },
+  {
+    "url": "runtime~main.801dfec724a2924f9e8f.bundle.js"
+  },
+  {
+    "revision": "790e30edce9f8564e926",
+    "url": "static/css/main.3bf0cbfd.chunk.css"
+  },
+  {
+    "revision": "acfa86b35c7f7921b86f",
+    "url": "static/css/vendors~main.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.801dfec724a2924f9e8f.bundle.js"
+  },
+  {
+    "url": "vendors~main.801dfec724a2924f9e8f.bundle.js.LICENSE.txt"
+  }
+]);
